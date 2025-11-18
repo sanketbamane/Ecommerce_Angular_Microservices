00b83b89e3133d3a99fa751fda771707a6b618a5
sanketbamane <69710838+sanketbamane@users.noreply.github.com>
2025-11-18 22:28:53 +0530

    Delete ecommerce-website directory

--- ecommerce-website/services/product-service/src/main/java/com/example/product/repository/ProductRepository.java
+++
@@ -1,5 +0,0 @@
-
-package com.example.product.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.product.model.Product;
-public interface ProductRepository extends JpaRepository<Product, Long> {}
